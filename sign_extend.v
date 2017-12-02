aadd8919bb9123694cbab9e9b6920ee0de4cf338
Pritheesh <p.priteesh@gmail.com>
2017-12-01 23:12:10 -0600

    new bug fixed

--- sign_extend.v
+++ sign_extend.v
@@ -4,6 +4,6 @@ module sign_extender(in, out);
   input  wire [15:0] in;
   output wire [31:0] out;
 
-  assign out = {{16{in[15]}, in[15:0]};
-  
+  assign out = {{16{in[15]}}, in[15:0]};
+
 endmodule
